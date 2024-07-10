7a6babe41cd801d1e0b0a877e01c06a2c765b4dd
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-10 15:01:03 +0530

    merge two sorted array

--- sorting/MergeSortedArray.java
+++ sorting/MergeSortedArray.java
@@ -1,11 +1,48 @@
+import java.util.Arrays;
+
 public class MergeSortedArray {
     public static void main(String[] args) {
-        
+        int[] arr1 = {1,2,3,0,0,0};
+        int[] arr2 = {1,2,2,3,5,6};
+        int ans[] = merge(arr1, arr1.length, arr2, arr2.length);
+        System.out.println(Arrays.toString(ans));
     }
 
-    static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] c = {};
+    static int[] merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] c = new int[m+n];
+        int i = 0 , j =0 , k = 0;
+        
+        while (i<m && j<n ) {
+            if (nums1[i] <= nums2[j]) {
+                // c[k] = nums1[i];
+                // i++;
+                // k++;
+                // or we can write it like 
+                c[k++] = nums1[i++];
+            } else {
+                // c[k] = nums2[j];
+                // j++;
+                // k++;
+                c[k++] = nums2[j++];
+            }
+        }
 
-      
+        // for (; i< m; i++) {
+        //     c[k] = nums1[i];
+        //     k++;
+        // }
+        // for (; j< n; j++) {
+        //     c[k] = nums2[j];
+        //     k++;
+        // }
+
+        while (i<m && nums1[i] > 0) {
+            c[k++] = nums1[i++];
+        } 
+        while (j<n && nums2[j] > 0) {
+            c[k++] = nums2[j++];
+        }
+
+        return c;
     }
 }
--- sorting/MissingNumber.java
+++ sorting/MissingNumber.java
@@ -1,21 +1,13 @@
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int[] arr = {3,5,2,0,1};
-        int ans = cyclic(arr);
+        int[] arr = {3,2,0,1};
+        int ans = missing(arr);
         System.out.println(ans);
     }
 
-    static int cyclic(int[]arr){
-        int i =0;
-        while (i < arr.length) {
-            int correct = arr[i];
-            if (arr[i] < arr.length && arr[i] != arr[correct]) {
-                swap(arr,i,correct);
-            } else i++;
-        }
-        // search for missing number
-
+    static int missing(int[] arr){
+        cyclic(arr);
         for (int index = 0; index < arr.length; index++) {
             // this array is [0,n] thats why we are doing if (arr[index] != index) return index;
             // if array asks [1,n] then by the logic it wll be if (arr[i] != i + 1) return i +1;
@@ -23,12 +15,18 @@ public class MissingNumber {
                 return index;
             }
         } 
-
-        // case 2 
-
         return arr.length;
     }
 
+    static void cyclic(int[]arr){
+        int i =0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr,i,correct);
+            } else i++;
+        }
+    }
 
     static void swap(int[]arr, int first, int second){
         int temp  = arr[first];
