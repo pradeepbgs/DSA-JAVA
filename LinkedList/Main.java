c20e4f1b2589b6da74d12d6887adccf229dbe7e0
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-07-17 17:49:24 +0530

    will do

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -2,20 +2,29 @@ package LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        LL list = new LL();
+        // LL list = new LL();
+        // list.insertFirst(3);
+        // list.insertFirst(5);
+        // list.insertFirst(7);
+        // list.insertFirst(9);
+        // list.insertFirst(22);
+        // list.insertLast(999);
+        // list.insert(1000, 3);
+        // list.display();
+        // System.out.println(list.deleteFirst());
+        // System.out.println(list.deleteLast());
+        // System.out.println(list.delete(2));
+        // list.display();
+        // System.out.println(list.find(5));
+        // list.display();
+
+
+        DLL list = new DLL();
         list.insertFirst(3);
         list.insertFirst(5);
         list.insertFirst(7);
         list.insertFirst(9);
-        list.insertFirst(22);
-        list.insertLast(999);
-        list.insert(1000, 3);
-        list.display();
-        System.out.println(list.deleteFirst());
-        System.out.println(list.deleteLast());
-        System.out.println(list.delete(2));
-        list.display();
-        System.out.println(list.find(5));
+
         list.display();
     }
 }
